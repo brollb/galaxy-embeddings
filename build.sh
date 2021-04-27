fcc97ed639c6f022d3b1fa4558b28d5bad789b9d
Brian Broll <brian.broll@gmail.com>
2021-04-27 09:20:29 -0500

    Add a couple more plots

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 echo "const PlotData = {};" > data/index.js
 echo "PlotData.redshift = $(cat data/redshift.json);" >> data/index.js
 echo "PlotData.bptclass = $(cat data/bptclass.json);" >> data/index.js
+echo "PlotData.petroR90 = $(cat data/petroR90.json);" >> data/index.js
+echo "PlotData.lgm_tot_p50 = $(cat data/lgm_tot_p50.json);" >> data/index.js
 echo "const PointMetadata = $(cat data/metadata.json);" >> data/index.js
