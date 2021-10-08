cd17d6578362c0895cb76af5dbe9907709d6f92e
Brian Broll <brian.broll@gmail.com>
2021-10-08 09:06:34 -0500

    Add spectral data

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ echo "PlotData.redshift = $(cat data/redshift.json);" >> data/index.js
 echo "PlotData.bptclass = $(cat data/bptclass.json);" >> data/index.js
 echo "PlotData.petroR90 = $(cat data/petroR90.json);" >> data/index.js
 echo "PlotData.lgm_tot_p50 = $(cat data/lgm_tot_p50.json);" >> data/index.js
+echo "PlotData.spectral = $(cat data/spectral.json);" >> data/index.js
 echo "const PointMetadata = $(cat data/metadata.json);" >> data/index.js
